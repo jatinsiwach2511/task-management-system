063b93c78c08006fd597e3dc4b949f7d3dfb8db2
Siwach@2511 <jatin@memorres.com>
2025-04-01 15:26:51 +0530

    add : added reminders module migrations

---
+++ migrations/sqls/20250401094458-reminders-module-up.sql
@@ -0,0 +1,12 @@
+/* Replace with your SQL commands */
+CREATE TABLE reminders (
+    id BIGSERIAL PRIMARY KEY,
+    usertaskid BIGSERIAL NOT NULL REFERENCES usertasks(usertaskid) ON UPDATE CASCADE ON DELETE CASCADE,
+    reminder_time TIMESTAMPTZ NOT NULL,
+    message VARCHAR(250),
+    created_on TIMESTAMPTZ DEFAULT current_timestamp,
+    updated_on TIMESTAMPTZ DEFAULT current_timestamp,
+    status VARCHAR(10) NOT NULL DEFAULT 'pending' CHECK(status IN ('pending','sent','failed'))
+);
+
+CREATE TRIGGER update_reminders_modtime BEFORE UPDATE ON reminders FOR EACH ROW EXECUTE PROCEDURE update_updated_on_column();
